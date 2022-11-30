3787a58e9371028bf8f2a4b75880c1d1aeb6637b
huzaifak08 <hk7928042@gmail.com>
2022-11-30 13:39:49 +0500

    Database Delete Operation and Sign Up using Firebase

--- lib/home.dart
+++ lib/home.dart
@@ -24,151 +24,154 @@ class _HomePageState extends State<HomePage> {
     const duration = Duration(milliseconds: 300);
     print('build');
     return Scaffold(
-        body: NotificationListener<UserScrollNotification>(
-          onNotification: (notification) {
-            final ScrollDirection direction = notification.direction;
-            setState(() {
-              if (direction == ScrollDirection.reverse) {
-                _showFab = false;
-              } else if (direction == ScrollDirection.forward) {
-                _showFab = true;
-              }
-            });
-            return true;
-          },
-          child: SafeArea(
-            child: Stack(
-              children: [
-                Image.asset(
-                  'assets/think.jpg',
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 200),
-                  child: Container(
-                    height: double.infinity,
-                    width: double.infinity,
-                    // color: Colors.red,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(35),
-                      ),
-                      color: Color.fromARGB(255, 231, 234, 235),
+      body: NotificationListener<UserScrollNotification>(
+        onNotification: (notification) {
+          final ScrollDirection direction = notification.direction;
+          setState(() {
+            if (direction == ScrollDirection.reverse) {
+              _showFab = false;
+            } else if (direction == ScrollDirection.forward) {
+              _showFab = true;
+            }
+          });
+          return true;
+        },
+        child: SafeArea(
+          child: Stack(
+            children: [
+              Image.asset(
+                'assets/think.jpg',
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 200),
+                child: Container(
+                  height: double.infinity,
+                  width: double.infinity,
+                  // color: Colors.red,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(35),
                     ),
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              'assets/bell.png',
-                              height: 50,
-                            ),
-                            SizedBox(width: 8),
-                            Text(
-                              'REMINDERS',
-                              style: TextStyle(
-                                fontSize: 35,
-                                height: 2,
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xFFF2796B),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Expanded(
-                          child: FirebaseAnimatedList(
-                            query: databaseRef,
-                            defaultChild: Center(
-                              child: CircularProgressIndicator(
-                                color: Color(0xFFF2796B),
-                              ),
-                            ),
-                            itemBuilder: (context, snapshot, animation, index) {
-                              String id = snapshot.child('id').value.toString();
-                              String title =
-                                  snapshot.child('title').value.toString();
-                              String description = snapshot
-                                  .child('description')
-                                  .value
-                                  .toString();
-                              return ListTile(
-                                  title: Text(
-                                    title,
-                                  ),
-                                  subtitle: Text(
-                                    description,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                  leading: CircleAvatar(
-                                    child: Text('HK'),
-                                    backgroundColor: Color(0xFFF2796B),
-                                  ),
-                                  trailing: PopupMenuButton(
-                                    icon: Icon(Icons.more_vert),
-                                    itemBuilder: (context) => [
-                                      PopupMenuItem(
-                                        value: 1,
-                                        child: ListTile(
-                                          title: Text('Edit'),
-                                          leading: Icon(Icons.edit),
-                                          onTap: () {
-                                            // This will cancel the Dialog Box:
-                                            Navigator.pop(context);
-
-                                            // and will move to next page:
-                                            Navigator.push(
-                                                context,
-                                                MaterialPageRoute(
-                                                    builder: (context) =>
-                                                        EditReminderPage(
-                                                          id: id,
-                                                          oldTitle: title,
-                                                          OldDescription:
-                                                              description,
-                                                        )));
-                                          },
-                                        ),
-                                      ),
-                                      PopupMenuItem(
-                                        value: 1,
-                                        child: ListTile(
-                                          title: Text('Delete'),
-                                          leading: Icon(Icons.delete),
-                                          onTap: () {
-                                            Navigator.pop(context);
-                                          },
-                                        ),
-                                      )
-                                    ],
-                                  ));
-                            },
-                          ),
-                        )
-                      ],
-                    ),
+                    color: Color.fromARGB(255, 231, 234, 235),
                   ),
-                )
-              ],
-            ),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/bell.png',
+                            height: 50,
+                          ),
+                          SizedBox(width: 8),
+                          Text(
+                            'REMINDERS',
+                            style: TextStyle(
+                              fontSize: 35,
+                              height: 2,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFFF2796B),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Expanded(
+                        child: FirebaseAnimatedList(
+                          query: databaseRef,
+                          defaultChild: Center(
+                            child: CircularProgressIndicator(
+                              color: Color(0xFFF2796B),
+                            ),
+                          ),
+                          itemBuilder: (context, snapshot, animation, index) {
+                            String id = snapshot.child('id').value.toString();
+                            String title =
+                                snapshot.child('title').value.toString();
+                            String description =
+                                snapshot.child('description').value.toString();
+                            return ListTile(
+                              title: Text(
+                                title,
+                              ),
+                              subtitle: Text(
+                                description,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              leading: CircleAvatar(
+                                backgroundImage: AssetImage("assets/bell.png"),
+                                backgroundColor: Color(0xFFF2796B),
+                              ),
+                              trailing: PopupMenuButton(
+                                icon: Icon(Icons.more_vert),
+                                itemBuilder: (context) => [
+                                  PopupMenuItem(
+                                    value: 1,
+                                    child: ListTile(
+                                      title: Text('Edit'),
+                                      leading: Icon(Icons.edit),
+                                      onTap: () {
+                                        // This will cancel the Dialog Box:
+                                        Navigator.pop(context);
+
+                                        // and will move to next page:
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    EditReminderPage(
+                                                      id: id,
+                                                      oldTitle: title,
+                                                      OldDescription:
+                                                          description,
+                                                    )));
+                                      },
+                                    ),
+                                  ),
+                                  PopupMenuItem(
+                                    value: 1,
+                                    child: ListTile(
+                                      title: Text('Delete'),
+                                      leading: Icon(Icons.delete),
+                                      onTap: () {
+                                        Navigator.pop(context);
+
+                                        // Firebase Delete:
+                                        databaseRef.child(id).remove();
+                                      },
+                                    ),
+                                  )
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
           ),
         ),
-        floatingActionButton: AnimatedSlide(
+      ),
+      floatingActionButton: AnimatedSlide(
+        duration: duration,
+        offset: _showFab ? Offset.zero : Offset(0, 2),
+        child: AnimatedOpacity(
           duration: duration,
-          offset: _showFab ? Offset.zero : Offset(0, 2),
-          child: AnimatedOpacity(
-            duration: duration,
-            opacity: _showFab ? 1 : 0,
-            child: FloatingActionButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => CreateReminderPage()));
-              },
-              child: Icon(Icons.add),
-              backgroundColor: Color(0xFFF2796B),
-            ),
+          opacity: _showFab ? 1 : 0,
+          child: FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => CreateReminderPage()));
+            },
+            child: Icon(Icons.add),
+            backgroundColor: Color(0xFFF2796B),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
--- lib/signup.dart
+++ lib/signup.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_learn/firebase_options.dart';
-import 'package:firebase_learn/home.dart';
 import 'package:firebase_learn/login.dart';
+import 'package:firebase_learn/round_button.dart';
+import 'package:firebase_learn/utils.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
@@ -14,190 +15,184 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
-  late final TextEditingController _email;
-  late final TextEditingController _password;
+  // For not accepting null form:
+  final _formKey = GlobalKey<FormState>();
 
-  @override
-  void initState() {
-    _email = TextEditingController();
-    _password = TextEditingController();
-    super.initState();
-  }
+  // Controllers:
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
 
-  @override
-  void dispose() {
-    _email.dispose();
-    _password.dispose();
-    super.dispose();
-  }
+  // Firebase Authentication:
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Loading:
+  bool loading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: FutureBuilder(
-      // Initialize Firebase:
-      future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform),
-
-      builder: (context, snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.done:
-            return SingleChildScrollView(
-              child: SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
-                  child: Column(
-                    // crossAxisAlignment: CrossAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+              child: Column(
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // SizedBox(height: 12),
+                  Row(
                     children: [
-                      // SizedBox(height: 12),
-                      Row(
-                        children: [
-                          Text(
-                            'Create Account',
-                            style: TextStyle(
-                              height: 2,
-                              fontSize: 23,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          SizedBox(width: 10),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 10.0),
-                            child: Image.asset(
-                              'assets/User.png',
-                              fit: BoxFit.cover,
-                            ),
-                          )
-                        ],
-                      ),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          'Make your life easier with us',
-                          style: TextStyle(color: Color(0xFF5B5B5B)),
-                          textAlign: TextAlign.start,
+                      Text(
+                        'Create Account',
+                        style: TextStyle(
+                          height: 2,
+                          fontSize: 23,
+                          fontWeight: FontWeight.w700,
                         ),
                       ),
-                      SizedBox(height: 30),
+                      SizedBox(width: 10),
                       Padding(
-                        padding: const EdgeInsets.only(left: 35.0),
-                        child: Container(
-                          height: 231,
-                          width: 296,
-                          child: Image.asset('assets/login_img.png'),
-                        ),
-                      ),
-                      SizedBox(height: 15),
-                      Text(
-                        'Enter your Email Address',
-                        style: TextStyle(
-                          color: Color(0xFFF2796B),
-                          fontWeight: FontWeight.w700,
-                          fontSize: 17,
-                        ),
-                      ),
-                      SizedBox(height: 12),
-                      TextField(
-                        controller: _email,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          hintText: 'someone@gmail.com',
-                        ),
-                        keyboardType: TextInputType.emailAddress,
-                      ),
-                      SizedBox(height: 15),
-                      Text(
-                        'Enter your Password',
-                        style: TextStyle(
-                          color: Color(0xFFF2796B),
-                          fontWeight: FontWeight.w700,
-                          fontSize: 17,
-                        ),
-                      ),
-                      SizedBox(height: 12),
-                      TextField(
-                        controller: _password,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          hintText: "abc123",
-                        ),
-                        keyboardType: TextInputType.visiblePassword,
-                        obscureText: true,
-                      ),
-                      SizedBox(height: 25),
-                      SizedBox(
-                        height: 43,
-                        width: 367,
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            // Firebase:
-                            final email = _email.text;
-                            final password = _password.text;
-
-                            try {
-                              // Firebase userCrendentials:
-                              final userCredential = await FirebaseAuth.instance
-                                  .createUserWithEmailAndPassword(
-                                      email: email, password: password);
-
-                              print(userCredential);
-
-                              // Navigate to Home Page:
-                              if (userCredential != null) {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                    builder: (context) => LoginPage(),
-                                  ),
-                                );
-                              }
-                            } on FirebaseAuthException catch (e) {
-                              if (e.code == 'invalid-email') {
-                                print('Invalid Email');
-                              } else if (e.code == 'email-already-in-use') {
-                                print('Email Already in Use');
-                              } else if (e.code == 'weak-password') {
-                                print('Weak Password');
-                              }
-                            }
-                          },
-                          child: Text('Sign UP'),
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Color(0xFFF2796B),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 12),
-                      RichText(
-                        text: TextSpan(
-                          style: TextStyle(color: Colors.grey),
-                          children: [
-                            TextSpan(text: 'Already have an account? '),
-                            TextSpan(
-                                text: 'Sign In',
-                                style: TextStyle(color: Colors.blue),
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = () {
-                                    Navigator.of(context)
-                                        .pushReplacement(MaterialPageRoute(
-                                      builder: (context) => LoginPage(),
-                                    ));
-                                  })
-                          ],
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Image.asset(
+                          'assets/User.png',
+                          fit: BoxFit.cover,
                         ),
                       )
                     ],
                   ),
-                ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Make your life easier with us',
+                      style: TextStyle(color: Color(0xFF5B5B5B)),
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 35.0),
+                    child: Container(
+                      height: 231,
+                      width: 296,
+                      child: Image.asset('assets/login_img.png'),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Text(
+                    'Enter your Email Address',
+                    style: TextStyle(
+                      color: Color(0xFFF2796B),
+                      fontWeight: FontWeight.w700,
+                      fontSize: 17,
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      hintText: 'someone@gmail.com',
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Enter your Email';
+                      } else
+                        null;
+                    },
+                  ),
+                  SizedBox(height: 15),
+                  Text(
+                    'Enter your Password',
+                    style: TextStyle(
+                      color: Color(0xFFF2796B),
+                      fontWeight: FontWeight.w700,
+                      fontSize: 17,
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      hintText: "abc123",
+                    ),
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Enter your Password';
+                      } else
+                        null;
+                    },
+                  ),
+                  SizedBox(height: 25),
+                  RoundButton(
+                    title: 'Sign UP',
+                    loading: loading,
+                    onTap: () {
+                      if (_formKey.currentState!.validate()) {
+                        signUp();
+                      }
+                    },
+                  ),
+                  SizedBox(height: 12),
+
+                  // Already have an Account?
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Already have an account? ',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context) => LoginPage(),
+                              ),
+                            );
+                          },
+                          child: Text(
+                            'Login In',
+                            style: TextStyle(color: Color(0xFFF2796B)),
+                          ))
+                    ],
+                  ),
+                ],
               ),
-            );
-          default:
-            return Text('Loading');
-        }
-      },
-    ));
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void signUp() {
+    setState(() {
+      loading = true;
+    });
+    _auth
+        .createUserWithEmailAndPassword(
+      email: _emailController.text.toString(),
+      password: _passwordController.text.toString(),
+    )
+        .then((value) {
+      setState(() {
+        loading = false;
+      });
+      Utils().toastMessage('Account Created Successfully');
+    }).onError((error, stackTrace) {
+      setState(() {
+        loading = false;
+      });
+      Utils().toastMessage(error.toString());
+    });
   }
 }
